eb7bf75d9a3da78c3398e7b2287dc4c9048fbd80
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-03-01 21:41:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,7 @@ def monopoly_with_second_tier
 	{
 		:railroads => {
 			:pieces => 4
-	}
+		}
 }
 end
 
@@ -25,8 +25,14 @@ def monopoly_with_third_tier
   # complexity.
 	{
 		:railroads => {
-			:pieces => 4
-	}
+			:pieces => 4,
+			:rent_in_dollars => {
+				:one_piece_owned => 25,
+				:two_pieces_owned => 50,
+				:three_pieces_owned => 100,
+				:four_pieces_owned => 200
+			}
+		}
 }
 end
 
